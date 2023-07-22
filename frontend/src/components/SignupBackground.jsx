72ab973813e48d543c2c78b587ac091cc25155f7
anandk1609 <krishnananand73@gmail.com>
2023-07-22 19:30:01 +0530

    Removed more unwanted assets from workspace

--- frontend/src/components/SignupBackground.jsx
+++ frontend/src/components/SignupBackground.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default function SignupBackground() {
-   return (<div></div>);
+   return (
+      <section className="signup__background">
+         <img src="https://assets.nflxext.com/ffe/siteui/vlv3/d7af077c-af5a-4055-8f9a-740a43588583/95bae10c-9773-4447-af4e-612a244231bd/IN-en-20230717-popsignuptwoweeks-perspective_alpha_website_small.jpg" alt="" className="" />
+      </section>
+   )
 };
